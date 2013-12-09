bb84a0f5639c3470d355d1924004102e54521e5f
Chris Aniszczyk <caniszczyk@gmail.com>
2013-12-09 09:35:21 -0600

    First release
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+= Twlinkfy = [![Build Status](https://travis-ci.org/caniszczyk/twlinkfy.png?branch=master)](https://travis-ci.org/caniszczyk/twlinkfy)
+
+A chrome extension to autolink Twitter usernames and hashtags
+
+== Authors ==
+
+* Chris Aniszczyk (@cra)
+
+== License ==
+Copyright 2013, Chris Aniszczyk
+
+Licensed under the Apache Public License 2.0
---
+++ ext/src/twlinkfy.js
@@ -0,0 +1,40 @@
+var elements = document.getElementsByTagName('body')[0];
+var validNodes = getNodes(elements);
+replaceNodes(validNodes);
+
+function replaceNodes(nodes) {
+var regex = new RegExp("^@\\w+", "g");
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        var newVal = node.nodeValue;
+        newVal = newVal.replace(regex, "<a href=\"https://twitter.com/intent/user?screen_name=$&\">" + '$&' + "</a>")
+        if (newVal !== node.nodeValue) {
+            var wrapper = document.createElement("span");
+            wrapper.innerHTML = newVal;
+            var parent = node.parentNode;
+            parent.insertBefore(wrapper, node);
+            parent.removeChild(node);
+        }
+    }
+}
+
+function getNodes(node) {
+    var textNodes = [];
+    var queue = [node];
+    var regex = new RegExp("^@\\w+", "g");
+    while (queue.length > 0) {
+        var childNode = queue.shift();
+	if (typeof childNode === "undefined")
+		continue;
+        var parent = childNode.parentElement;
+        if (typeof parent != "undefined" && childNode.nodeType === 3 && parent.tagName !== 'A' && parent.tagName !== 'TEXTAREA' && childNode.nodeValue.match(regex)) {
+            textNodes.push(childNode);
+        } else {
+            for (var i = 0; i < childNode.childNodes.length; i++) {
+                queue.push(childNode.childNodes[i]);
+            }
+        }
+    }
+
+    return textNodes;
+}
